24e4bd5b1132468bef6e20ec64e9544fb6bfe23f
Leiner <leinersacdalan4@gmail.com>
2024-04-20 20:53:44 +0800

    user login

--- src/api/endpoints/Users.ts
+++ src/api/endpoints/Users.ts
@@ -2,3 +2,4 @@ export const createData = '/Users/create/';
 export const readData = '/Users/get/';
 export const updateData = '/Users/update/';
 export const deleteData = '/Users/delete/'; 
+export const loginData = '/Users/login'; 
